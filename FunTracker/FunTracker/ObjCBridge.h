5fa9db196bd61454e1967f934719063140ae7502
Mitchell Au <aumitche@gmail.com>
2015-10-03 00:13:58 -0400

    Set up the Estimote SDK

---
+++ FunTracker/FunTracker/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  FunTracker
+//
+//  Created by aumitche on 2015–10–02.
+//  Copyright © 2015 NIMF. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
