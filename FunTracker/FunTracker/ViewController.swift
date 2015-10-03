dff96e7cd7619d9a6dfb8a3b83b8f51618ac14e0
Mitchell Au <aumitche@gmail.com>
2015-10-03 12:23:56 -0400

    Established communication with Raspi server

--- FunTracker/FunTracker/ViewController.swift
+++ FunTracker/FunTracker/ViewController.swift
@@ -7,20 +7,44 @@
 //
 
 import UIKit
+import SwiftHTTP
 
 class ViewController: UIViewController {
-	let ip = "169.254.1.15"
+	let ip = "http://10.10.10.60/rec.php?ID=10&BPM=120&X=0&Y=30"
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		do {
+			let opt = try HTTP.GET(ip)
+			opt.start { response in
+				if let err = response.error {
+					print("error: \(err.localizedDescription)")
+					return //also notify app of failure as needed
+				}
+				//print("opt finished: \(response.description)")
+				//print("\(response.data)") //access the response of the data with response.data
+				var data = NSString(data: response.data, encoding: NSUTF8StringEncoding)
+				print(data!)
+			}
+		} catch let error {
+			print("got an error creating the request: \(error)")
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	func initialComm() {
+		
+	}
 
-
+	func communicate(id: NSInteger, bpm: NSInteger, x: NSInteger, y: NSInteger) {
+		
+	}
+	
 }
 
